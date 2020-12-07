0aea1d3b639a6e949157a122e3d5f0b4fc6d1473
Victor Kareh <vkareh@redhat.com>
2020-12-07 13:19:50 -0500

    user: Determine if user exists before revoking

--- pkg/ocm/helpers.go
+++ pkg/ocm/helpers.go
@@ -20,6 +20,7 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"net/http"
 	"regexp"
 
 	sdk "github.com/openshift-online/ocm-sdk-go"
@@ -124,6 +125,21 @@ func GetIngresses(client *cmv1.ClustersClient, clusterID string) ([]*cmv1.Ingres
 	}
 
 	return response.Items().Slice(), nil
+}
+
+func GetUser(client *cmv1.ClustersClient, clusterID string, group string, username string) (*cmv1.User, error) {
+	response, err := client.Cluster(clusterID).
+		Groups().Group(group).
+		Users().User(username).
+		Get().Send()
+	if err != nil {
+		if response.Status() == http.StatusNotFound {
+			return nil, nil
+		}
+		return nil, handleErr(response.Error(), err)
+	}
+
+	return response.Body(), nil
 }
 
 func GetUsers(client *cmv1.ClustersClient, clusterID string, group string) ([]*cmv1.User, error) {
