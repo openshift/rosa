d218cef65a776783fd458ee5a1d9cda9132ed606
Victor Kareh <vkareh@redhat.com>
2021-01-08 10:05:07 -0500

    addons: Support add-on installation parameters
    Since some add-ons require custom parameters before installation, we
    iterate through the list generating interactive prompts for those
    parameters. Those parameters are then sent as part of the payload to the
    add-on installation API and will be available in the cluster's addon
    target namespace.

--- cmd/describe/addon/cmd.go
+++ cmd/describe/addon/cmd.go
@@ -22,6 +22,7 @@ import (
 	"regexp"
 	"strings"
 
+	cmv1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
 	"github.com/spf13/cobra"
 
 	"github.com/openshift/rosa/pkg/logging"
@@ -81,7 +82,7 @@ func run(_ *cobra.Command, argv []string) {
 	}
 
 	// Print add-on description:
-	fmt.Printf(""+
+	fmt.Printf("ADD-ON\n"+
 		"ID:               %s\n"+
 		"Name:             %s\n"+
 		"Description:      %s\n"+
@@ -98,6 +99,44 @@ func run(_ *cobra.Command, argv []string) {
 		addOn.InstallMode(),
 	)
 	fmt.Println()
+
+	if addOn.Parameters().Len() > 0 {
+		fmt.Printf("ADD-ON PARAMETERS\n")
+		addOn.Parameters().Each(func(param *cmv1.AddOnParameter) bool {
+			if !param.Enabled() {
+				return true
+			}
+			fmt.Printf(""+
+				"- ID:             %s\n"+
+				"  Name:           %s\n"+
+				"  Description:    %s\n"+
+				"  Type:           %s\n"+
+				"  Required:       %s\n"+
+				"  Editable:       %s\n",
+				param.ID(),
+				param.Name(),
+				wrapText(param.Description()),
+				param.ValueType(),
+				printBool(param.Required()),
+				printBool(param.Editable()),
+			)
+			if param.DefaultValue() != "" {
+				fmt.Printf("  Default Value:  %s\n", param.DefaultValue())
+			}
+			if param.Validation() != "" {
+				fmt.Printf("  Validation:     /%s/\n", param.Validation())
+			}
+			fmt.Println()
+			return true
+		})
+	}
+}
+
+func printBool(val bool) string {
+	if val {
+		return "yes"
+	}
+	return "no"
 }
 
 func wrapText(text string) string {
