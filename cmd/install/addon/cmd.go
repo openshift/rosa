fd4181f324ae2ceae82a914b5421488237a69e53
Victor Kareh <vkareh@redhat.com>
2021-01-08 10:05:07 -0500

    addons: Support add-on installation parameters
    Since some add-ons require custom parameters before installation, we
    iterate through the list generating interactive prompts for those
    parameters. Those parameters are then sent as part of the payload to the
    add-on installation API and will be available in the cluster's addon
    target namespace.

--- cmd/install/addon/cmd.go
+++ cmd/install/addon/cmd.go
@@ -17,7 +17,10 @@ limitations under the License.
 package addon
 
 import (
+	"net"
 	"os"
+	"regexp"
+	"strconv"
 
 	cmv1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
 	"github.com/spf13/cobra"
@@ -25,6 +28,7 @@ import (
 	"github.com/openshift/rosa/pkg/aws"
 	clusterprovider "github.com/openshift/rosa/pkg/cluster"
 	"github.com/openshift/rosa/pkg/confirm"
+	"github.com/openshift/rosa/pkg/interactive"
 	"github.com/openshift/rosa/pkg/logging"
 	"github.com/openshift/rosa/pkg/ocm"
 	rprtr "github.com/openshift/rosa/pkg/reporter"
@@ -117,11 +121,11 @@ func run(_ *cobra.Command, argv []string) {
 	}()
 
 	// Get the client for the OCM collection of clusters:
-	clustersCollection := ocmConnection.ClustersMgmt().V1().Clusters()
+	ocmClient := ocmConnection.ClustersMgmt().V1()
 
 	// Try to find the cluster:
 	reporter.Debugf("Loading cluster '%s'", clusterKey)
-	cluster, err := ocm.GetCluster(clustersCollection, clusterKey, awsCreator.ARN)
+	cluster, err := ocm.GetCluster(ocmClient.Clusters(), clusterKey, awsCreator.ARN)
 	if err != nil {
 		reporter.Errorf("Failed to get cluster '%s': %v", clusterKey, err)
 		os.Exit(1)
@@ -132,15 +136,71 @@ func run(_ *cobra.Command, argv []string) {
 		os.Exit(1)
 	}
 
-	reporter.Warnf("Once installed, add-ons cannot be uninstalled")
-	if confirm.Confirm("install add-on '%s' on cluster '%s'", addOnID, clusterKey) {
-		reporter.Debugf("Installing add-on '%s' on cluster '%s'", addOnID, clusterKey)
-		err = clusterprovider.InstallAddOn(clustersCollection, clusterKey, awsCreator.ARN, addOnID)
-		if err != nil {
-			reporter.Errorf("Failed to add add-on installation '%s' for cluster '%s': %s", addOnID, clusterKey, err)
-			os.Exit(1)
-		}
-		reporter.Infof("Add-on '%s' is now installing. To check the status run 'rosa list addons -c %s'",
-			addOnID, clusterKey)
+	if !confirm.Confirm("install add-on '%s' on cluster '%s'", addOnID, clusterKey) {
+		os.Exit(0)
 	}
+
+	parameters, err := clusterprovider.GetAddOnParameters(ocmClient.Addons(), addOnID)
+	if err != nil {
+		reporter.Errorf("Failed to get add-on '%s' parameters: %v", addOnID, err)
+		os.Exit(1)
+	}
+
+	var params []clusterprovider.AddOnParam
+	if parameters.Len() > 0 {
+		parameters.Each(func(param *cmv1.AddOnParameter) bool {
+			input := interactive.Input{
+				Question: param.Name(),
+				Help:     param.Description(),
+				Default:  param.DefaultValue(),
+				Required: param.Required(),
+			}
+
+			var val string
+			switch param.ValueType() {
+			case "boolean":
+				var boolVal bool
+				boolVal, err = interactive.GetBool(input)
+				if boolVal {
+					val = "true"
+				} else {
+					val = "false"
+				}
+			case "cidr":
+				var cidrVal net.IPNet
+				cidrVal, err = interactive.GetIPNet(input)
+				val = cidrVal.String()
+			case "number":
+				var numVal int
+				numVal, err = interactive.GetInt(input)
+				val = strconv.Itoa(numVal)
+			case "string":
+				val, err = interactive.GetString(input)
+			}
+			if err != nil {
+				reporter.Errorf("Expected a valid value for '%s': %v", param.ID(), err)
+				os.Exit(1)
+			}
+
+			if param.Validation() != "" {
+				isValid, err := regexp.MatchString(param.Validation(), val)
+				if err != nil || !isValid {
+					reporter.Errorf("Expected %v to match /%s/", val, param.Validation())
+					os.Exit(1)
+				}
+			}
+
+			params = append(params, clusterprovider.AddOnParam{Key: param.ID(), Val: val})
+
+			return true
+		})
+	}
+
+	reporter.Debugf("Installing add-on '%s' on cluster '%s'", addOnID, clusterKey)
+	err = clusterprovider.InstallAddOn(ocmClient.Clusters(), clusterKey, awsCreator.ARN, addOnID, params)
+	if err != nil {
+		reporter.Errorf("Failed to add add-on installation '%s' for cluster '%s': %v", addOnID, clusterKey, err)
+		os.Exit(1)
+	}
+	reporter.Infof("Add-on '%s' is now installing. To check the status run 'rosa list addons -c %s'", addOnID, clusterKey)
 }
--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -285,15 +285,39 @@ func DeleteCluster(client *cmv1.ClustersClient, clusterKey string, creatorARN st
 	return cluster, nil
 }
 
-func InstallAddOn(client *cmv1.ClustersClient, clusterKey string, creatorARN string, addOnID string) error {
+func GetAddOnParameters(client *cmv1.AddOnsClient, addOnID string) (*cmv1.AddOnParameterList, error) {
+	response, err := client.Addon(addOnID).Get().Send()
+	if err != nil {
+		return nil, handleErr(response.Error(), err)
+	}
+	return response.Body().Parameters(), nil
+}
+
+type AddOnParam struct {
+	Key string
+	Val string
+}
+
+func InstallAddOn(client *cmv1.ClustersClient, clusterKey string, creatorARN string, addOnID string,
+	params []AddOnParam) error {
 	cluster, err := GetCluster(client, clusterKey, creatorARN)
 	if err != nil {
 		return err
 	}
 
-	addOnInstallation, err := cmv1.NewAddOnInstallation().
-		Addon(cmv1.NewAddOn().ID(addOnID)).
-		Build()
+	addOnInstallationBuilder := cmv1.NewAddOnInstallation().
+		Addon(cmv1.NewAddOn().ID(addOnID))
+
+	if len(params) > 0 {
+		addOnParamList := make([]*cmv1.AddOnInstallationParameterBuilder, len(params))
+		for i, param := range params {
+			addOnParamList[i] = cmv1.NewAddOnInstallationParameter().ID(param.Key).Value(param.Val)
+		}
+		addOnInstallationBuilder = addOnInstallationBuilder.
+			Parameters(cmv1.NewAddOnInstallationParameterList().Items(addOnParamList...))
+	}
+
+	addOnInstallation, err := addOnInstallationBuilder.Build()
 	if err != nil {
 		return err
 	}
