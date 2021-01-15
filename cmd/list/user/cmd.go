3c059c92f3d73118edbf85f4897eb5f1b9f8da07
Victor Kareh <vkareh@redhat.com>
2021-01-15 09:09:34 -0500

    cluster-admins: Remove explicit enable
    Because adding a user to cluster-admins already requires manual
    intervention, enabling it first seems superfluous and can lead to
    customer friction. We remove this requirement and the associated flag,
    and immediately allow customers to grant users the cluster-admin role
    without the need for an explicit opt-in for a cluster.

--- cmd/list/user/cmd.go
+++ cmd/list/user/cmd.go
@@ -121,19 +121,17 @@ func run(_ *cobra.Command, _ []string) {
 	}
 
 	var clusterAdmins []*cmv1.User
-	if cluster.ClusterAdminEnabled() {
-		reporter.Debugf("Loading users for cluster '%s'", clusterKey)
-		// Load cluster-admins for this cluster
-		clusterAdmins, err = ocm.GetUsers(clustersCollection, cluster.ID(), "cluster-admins")
-		if err != nil {
-			reporter.Errorf("Failed to get cluster-admins for cluster '%s': %v", clusterKey, err)
-			os.Exit(1)
-		}
-		// Remove cluster-admin user
-		for i, user := range clusterAdmins {
-			if user.ID() == "cluster-admin" {
-				clusterAdmins = append(clusterAdmins[:i], clusterAdmins[i+1:]...)
-			}
+	reporter.Debugf("Loading users for cluster '%s'", clusterKey)
+	// Load cluster-admins for this cluster
+	clusterAdmins, err = ocm.GetUsers(clustersCollection, cluster.ID(), "cluster-admins")
+	if err != nil {
+		reporter.Errorf("Failed to get cluster-admins for cluster '%s': %v", clusterKey, err)
+		os.Exit(1)
+	}
+	// Remove cluster-admin user
+	for i, user := range clusterAdmins {
+		if user.ID() == "cluster-admin" {
+			clusterAdmins = append(clusterAdmins[:i], clusterAdmins[i+1:]...)
 		}
 	}
 
