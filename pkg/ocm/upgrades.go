53f2ab0dd72bbe0c6e4ce65e7ebbebda0074f42f
pvasanth <pvasanth@redhat.com>
2022-01-20 14:57:52 -0500

    Add version gate ackto ROSA

--- pkg/ocm/upgrades.go
+++ pkg/ocm/upgrades.go
@@ -17,8 +17,7 @@ limitations under the License.
 package ocm
 
 import (
-	"fmt"
-
+	"encoding/json"
 	cmv1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
 )
 
@@ -51,7 +50,6 @@ func (c *Client) GetScheduledUpgrade(clusterID string) (*cmv1.UpgradePolicy, *cm
 	if err != nil {
 		return nil, nil, err
 	}
-
 	for _, upgradePolicy := range upgradePolicies {
 		if upgradePolicy.ScheduleType() == "manual" && upgradePolicy.UpgradeType() == "OSD" {
 			state, err := c.ocm.ClustersMgmt().V1().
@@ -102,49 +100,33 @@ func (c *Client) CancelUpgrade(clusterID string) (bool, error) {
 func (c *Client) GetMissingGateAgreements(
 	clusterID string,
 	upgradePolicy *cmv1.UpgradePolicy) ([]*cmv1.VersionGate, error) {
-	response, err := c.ocm.ClustersMgmt().V1().
-		Clusters().Cluster(clusterID).
-		UpgradePolicies().
-		Add().
-		Parameter("dryRun", true).
-		Body(upgradePolicy).
-		Send()
-	if err == nil {
-		return []*cmv1.VersionGate{}, nil
-	}
+	response, err := c.ocm.ClustersMgmt().V1().Clusters().
+		Cluster(clusterID).UpgradePolicies().Add().Parameter("dryRun", true).Body(upgradePolicy).Send()
 
-	// parse gates list
-	details, ok := response.Error().GetDetails()
-	if !ok {
-		return []*cmv1.VersionGate{}, handleErr(response.Error(), err)
-	}
-
-	listOfMaps, ok := details.([]interface{})
-	if !ok {
-		return []*cmv1.VersionGate{}, fmt.Errorf("Failed to parse gates")
-	}
-
-	gates := make([]*cmv1.VersionGate, 0)
-	for _, object := range listOfMaps {
-		gateAsMap, ok := object.(map[string]interface{})
-		if !ok {
-			return []*cmv1.VersionGate{}, handleErr(response.Error(), err)
+	if err != nil {
+		if response.Error() != nil {
+			// parse gates list
+			errorDetails, ok := response.Error().GetDetails()
+			if !ok {
+				return []*cmv1.VersionGate{}, handleErr(response.Error(), err)
+			}
+			data, err := json.Marshal(errorDetails)
+			if err != nil {
+				return []*cmv1.VersionGate{}, handleErr(response.Error(), err)
+			}
+			gates, err := cmv1.UnmarshalVersionGateList(data)
+			if err != nil {
+				return []*cmv1.VersionGate{}, handleErr(response.Error(), err)
+			}
+			return gates, nil
 		}
-
-		gate, err := gateFromMap(gateAsMap)
-		if err != nil {
-			return []*cmv1.VersionGate{}, fmt.Errorf("failed to read gate")
-		}
-		gates = append(gates, gate)
 	}
-
-	return gates, nil
+	return []*cmv1.VersionGate{}, nil
 }
 
 func (c *Client) AckVersionGate(
 	clusterID string,
 	gateID string) error {
-
 	agreement, err := cmv1.NewVersionGateAgreement().
 		VersionGate(cmv1.NewVersionGate().ID(gateID)).
 		Build()
@@ -161,16 +143,4 @@ func (c *Client) AckVersionGate(
 		return handleErr(response.Error(), err)
 	}
 	return nil
-}
-
-func gateFromMap(gateMap map[string]interface{}) (*cmv1.VersionGate, error) {
-	id := gateMap["id"].(string)
-	desc := gateMap["description"].(string)
-	stsOnly := gateMap["sts_only"].(bool)
-
-	gate, err := cmv1.NewVersionGate().ID(id).Description(desc).STSOnly(stsOnly).Build()
-	if err != nil {
-		return nil, err
-	}
-	return gate, nil
 }
