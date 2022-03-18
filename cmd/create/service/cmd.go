759f274c00bb417c6db05bafed3a5f972d16ed51
Renan Campos <rcampos@redhat.com>
2022-03-18 11:44:50 -0400

    Only supporting STS for service create for now.
    Calling service create with AWS credentials invalidates the credentials!
    TODO: Managed service parameters through command line. Hard-coded them
    for current testing.

--- cmd/create/service/cmd.go
+++ cmd/create/service/cmd.go
@@ -17,13 +17,18 @@ limitations under the License.
 package service
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
+	"github.com/aws/aws-sdk-go/aws/arn"
 	"github.com/spf13/cobra"
 
 	"github.com/openshift/rosa/pkg/aws"
+	"github.com/openshift/rosa/pkg/interactive"
 	"github.com/openshift/rosa/pkg/logging"
 	"github.com/openshift/rosa/pkg/ocm"
+	"github.com/openshift/rosa/pkg/output"
 	rprtr "github.com/openshift/rosa/pkg/reporter"
 )
 
@@ -80,20 +85,163 @@ func run(cmd *cobra.Command, _ []string) {
 
 	awsClient := aws.GetAWSClientForUserRegion(reporter, logger)
 
+	// Openshift version to use.
+	// Hard-coding 4.9 for now
+	version := "4.9"
+	minor := ocm.GetVersionMinor(version)
+	role := aws.AccountRoles[aws.InstallerAccountRole]
+
+	// Find all installer roles in the current account using AWS resource tags
+	var roleARN string
+	var supportRoleARN string
+	var controlPlaneRoleARN string
+	var workerRoleARN string
+	var hasRoles bool
+
+	roleARNs, err := awsClient.FindRoleARNs(aws.InstallerAccountRole, minor)
+	if err != nil {
+		reporter.Errorf("Failed to find %s role: %s", role.Name, err)
+		os.Exit(1)
+	}
+
+	if len(roleARNs) > 1 {
+		defaultRoleARN := roleARNs[0]
+		// Prioritize roles with the default prefix
+		for _, rARN := range roleARNs {
+			if strings.Contains(rARN, fmt.Sprintf("%s-%s-Role", aws.DefaultPrefix, role.Name)) {
+				defaultRoleARN = rARN
+			}
+		}
+		reporter.Warnf("More than one %s role found, going with %s", role.Name, defaultRoleARN)
+		roleARN = defaultRoleARN
+	} else if len(roleARNs) == 1 {
+		if !output.HasFlag() || reporter.IsTerminal() {
+			reporter.Infof("Using %s for the %s role", roleARNs[0], role.Name)
+		}
+		roleARN = roleARNs[0]
+	} else {
+		reporter.Errorf("No account roles found. " +
+			"You will need to run 'rosa create account-roles' to create them first.")
+	}
+
+	if roleARN != "" {
+		// Get role prefix
+		rolePrefix, err := getAccountRolePrefix(roleARN, role)
+		if err != nil {
+			reporter.Errorf("Failed to find prefix from %s account role", role.Name)
+			os.Exit(1)
+		}
+		reporter.Debugf("Using '%s' as the role prefix", rolePrefix)
+
+		hasRoles = true
+		for roleType, role := range aws.AccountRoles {
+			if roleType == aws.InstallerAccountRole {
+				// Already dealt with
+				continue
+			}
+			roleARNs, err := awsClient.FindRoleARNs(roleType, minor)
+			if err != nil {
+				reporter.Errorf("Failed to find %s role: %s", role.Name, err)
+				os.Exit(1)
+			}
+			selectedARN := ""
+			for _, rARN := range roleARNs {
+				if strings.Contains(rARN, fmt.Sprintf("%s-%s-Role", rolePrefix, role.Name)) {
+					selectedARN = rARN
+				}
+			}
+			if selectedARN == "" {
+				reporter.Errorf("No %s account roles found. "+
+					"You will need to run 'rosa create account-roles' to create them first.",
+					role.Name)
+				interactive.Enable()
+				hasRoles = false
+			}
+			if !output.HasFlag() || reporter.IsTerminal() {
+				reporter.Infof("Using %s for the %s role", selectedARN, role.Name)
+			}
+			switch roleType {
+			case aws.InstallerAccountRole:
+				roleARN = selectedARN
+			case aws.SupportAccountRole:
+				supportRoleARN = selectedARN
+			case aws.ControlPlaneAccountRole:
+				controlPlaneRoleARN = selectedARN
+			case aws.WorkerAccountRole:
+				workerRoleARN = selectedARN
+			}
+		}
+	}
+	if hasRoles == false {
+		reporter.Errorf("Please create the above roles to continue")
+		os.Exit(1)
+	}
+
+	args.AwsRoleARN = roleARN
+	args.AwsSupportRoleARN = supportRoleARN
+	args.AwsControlPlaneRoleARN = controlPlaneRoleARN
+	args.AwsWorkerRoleARN = workerRoleARN
+
+	// operator role logic.
 	awsCreator, err := awsClient.GetCreator()
 	if err != nil {
 		reporter.Errorf("Unable to get IAM credentials: %v", err)
 		os.Exit(1)
 	}
 
-	accessKey, err := awsClient.GetAWSAccessKeys()
-	if err != nil {
-		reporter.Errorf("Unable to get access keys: %v", err)
-		os.Exit(1)
+	operatorRolesPrefix := getRolePrefix(args.ClusterName)
+	operatorIAMRoleList := []ocm.OperatorIAMRole{}
+
+	for _, operator := range aws.CredentialRequests {
+		//If the cluster version is less than the supported operator version
+		if operator.MinVersion != "" {
+			isSupported, err := ocm.CheckSupportedVersion(ocm.GetVersionMinor(version), operator.MinVersion)
+			if err != nil {
+				reporter.Errorf("Error validating operator role '%s' version %s", operator.Name, err)
+				os.Exit(1)
+			}
+			if !isSupported {
+				continue
+			}
+		}
+		operatorIAMRoleList = append(operatorIAMRoleList, ocm.OperatorIAMRole{
+			Name:      operator.Name,
+			Namespace: operator.Namespace,
+			RoleARN:   getOperatorRoleArn(operatorRolesPrefix, operator, awsCreator),
+		})
 	}
+
+	// Validate the role names are available on AWS
+	for _, role := range operatorIAMRoleList {
+		name := strings.SplitN(role.RoleARN, "/", 2)[1]
+		err := awsClient.ValidateRoleNameAvailable(name)
+		if err != nil {
+			reporter.Errorf("Error validating role: %v", err)
+			os.Exit(1)
+		}
+	}
+
+	args.AwsOperatorIamRoleList = operatorIAMRoleList
+	// end operator role logic.
+
+	/*
+		awsCreator, err := awsClient.GetCreator()
+		if err != nil {
+			reporter.Errorf("Unable to get IAM credentials: %v", err)
+			os.Exit(1)
+		}
+
+		accessKey, err := awsClient.GetAWSAccessKeys()
+		if err != nil {
+			reporter.Errorf("Unable to get access keys: %v", err)
+			os.Exit(1)
+		}
+		args.AwsAccountID = awsCreator.AccountID
+		args.AwsAccessKeyID = accessKey.AccessKeyID
+		args.AwsSecretAccessKey = accessKey.SecretAccessKey
+	*/
+
 	args.AwsAccountID = awsCreator.AccountID
-	args.AwsAccessKeyID = accessKey.AccessKeyID
-	args.AwsSecretAccessKey = accessKey.SecretAccessKey
 
 	// Get AWS region
 	args.AwsRegion, err = aws.GetRegion("")
@@ -101,10 +249,43 @@ func run(cmd *cobra.Command, _ []string) {
 		reporter.Errorf("Error getting region: %v", err)
 		os.Exit(1)
 	}
+	reporter.Infof("Using AWS region: %s", args.AwsRegion)
 
+	// Creating the service
 	_, err = ocmClient.CreateManagedService(args)
 	if err != nil {
 		reporter.Errorf("Failed to create managed service: %s", err)
 		os.Exit(1)
 	}
+
+	// The client must run these rosa commands after this for the cluster to properly install.
+	rolesCMD := fmt.Sprintf("rosa create operator-roles --cluster %s", args.ClusterName)
+	oidcCMD := fmt.Sprintf("rosa create oidc-provider --cluster %s", args.ClusterName)
+
+	reporter.Infof("Run the following commands to continue the cluster creation:\n\n"+
+		"\t%s\n"+
+		"\t%s\n",
+		rolesCMD, oidcCMD)
+}
+
+func getAccountRolePrefix(roleARN string, role aws.AccountRole) (string, error) {
+	parsedARN, err := arn.Parse(roleARN)
+	if err != nil {
+		return "", err
+	}
+	roleName := strings.SplitN(parsedARN.Resource, "/", 2)[1]
+	rolePrefix := strings.TrimSuffix(roleName, fmt.Sprintf("-%s-Role", role.Name))
+	return rolePrefix, nil
+}
+
+func getRolePrefix(clusterName string) string {
+	return fmt.Sprintf("%s-%s", clusterName, ocm.RandomLabel(4))
+}
+
+func getOperatorRoleArn(prefix string, operator aws.Operator, creator *aws.Creator) string {
+	role := fmt.Sprintf("%s-%s-%s", prefix, operator.Namespace, operator.Name)
+	if len(role) > 64 {
+		role = role[0:64]
+	}
+	return fmt.Sprintf("arn:aws:iam::%s:role/%s", creator.AccountID, role)
 }
--- pkg/ocm/managedservice.go
+++ pkg/ocm/managedservice.go
@@ -9,16 +9,39 @@ type CreateManagedServiceArgs struct {
 	ServiceName string
 	ClusterName string
 
-	AwsAccountID       string
-	AwsAccessKeyID     string
-	AwsSecretAccessKey string
-	AwsRegion          string
+	AwsAccountID           string
+	AwsAccessKeyID         string
+	AwsSecretAccessKey     string
+	AwsRoleARN             string
+	AwsSupportRoleARN      string
+	AwsControlPlaneRoleARN string
+	AwsWorkerRoleARN       string
+	AwsRegion              string
+
+	AwsOperatorIamRoleList []OperatorIAMRole
 }
 
 func (c *Client) CreateManagedService(args CreateManagedServiceArgs) (*msv1.ManagedService, error) {
 
+	operatorIamRoles := []*msv1.OperatorIAMRoleBuilder{}
+	for _, operatorIAMRole := range args.AwsOperatorIamRoleList {
+		operatorIamRoles = append(operatorIamRoles,
+			msv1.NewOperatorIAMRole().
+				Name(operatorIAMRole.Name).
+				Namespace(operatorIAMRole.Namespace).
+				RoleARN(operatorIAMRole.RoleARN))
+	}
+
 	service, err := msv1.NewManagedService().
 		Service(args.ServiceName).
+		Parameters(
+			msv1.NewServiceParameter().
+				ID("has-external-resources").
+				Value("true"),
+			msv1.NewServiceParameter().
+				ID("parameter-with-requirements").
+				Value("1"),
+		).
 		Cluster(
 			msv1.NewCluster().
 				Name(args.ClusterName).
@@ -27,6 +50,13 @@ func (c *Client) CreateManagedService(args CreateManagedServiceArgs) (*msv1.Mana
 						ID(args.AwsRegion)).
 				AWS(
 					msv1.NewAWS().
+						STS(msv1.NewSTS().
+							RoleARN(args.AwsRoleARN).
+							SupportRoleARN(args.AwsSupportRoleARN).
+							InstanceIAMRoles(msv1.NewInstanceIAMRoles().
+								MasterRoleARN(args.AwsControlPlaneRoleARN).
+								WorkerRoleARN(args.AwsWorkerRoleARN)).
+							OperatorIAMRoles(operatorIamRoles...)).
 						AccountID(args.AwsAccountID).
 						AccessKeyID(args.AwsAccessKeyID).
 						SecretAccessKey(args.AwsSecretAccessKey))).
