710fbd1d5bac011ba7413125177b2883c2f638c0
Tomer Brisker <tbrisker@gmail.com>
2022-06-27 16:42:11 +0300

    Migrate edit service to use runtime

--- cmd/edit/service/cmd.go
+++ cmd/edit/service/cmd.go
@@ -23,9 +23,8 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/openshift/rosa/pkg/arguments"
-	"github.com/openshift/rosa/pkg/logging"
 	"github.com/openshift/rosa/pkg/ocm"
-	rprtr "github.com/openshift/rosa/pkg/reporter"
+	"github.com/openshift/rosa/pkg/rosa"
 )
 
 var args ocm.UpdateManagedServiceArgs
@@ -65,42 +64,27 @@ func init() {
 }
 
 func run(cmd *cobra.Command, argv []string) {
-	reporter := rprtr.CreateReporterOrExit()
-	logger := logging.NewLogger()
+	r := rosa.NewRuntime().WithOCM()
+	defer r.Cleanup()
 
 	if args.ID == "" {
-		reporter.Errorf("Service id not specified.")
+		r.Reporter.Errorf("Service id not specified.")
 		cmd.Help()
 		os.Exit(1)
 	}
 
-	// Create the client for the OCM API:
-	ocmClient, err := ocm.NewClient().
-		Logger(logger).
-		Build()
-	if err != nil {
-		reporter.Errorf("Failed to create OCM connection: %v", err)
-		os.Exit(1)
-	}
-	defer func() {
-		err = ocmClient.Close()
-		if err != nil {
-			reporter.Errorf("Failed to close OCM connection: %v", err)
-		}
-	}()
-
 	// Try to find the cluster:
-	reporter.Debugf("Loading service %q", args.ID)
-	service, err := ocmClient.GetManagedService(ocm.DescribeManagedServiceArgs{ID: args.ID})
+	r.Reporter.Debugf("Loading service %q", args.ID)
+	service, err := r.OCMClient.GetManagedService(ocm.DescribeManagedServiceArgs{ID: args.ID})
 	if err != nil {
-		reporter.Errorf("Failed to get service %q: %v", args.ID, err)
+		r.Reporter.Errorf("Failed to get service %q: %v", args.ID, err)
 		os.Exit(1)
 	}
 
 	parameters := service.Parameters()
 
 	if len(parameters) == 0 {
-		reporter.Errorf("Service %q has no parameters to edit", args.ID)
+		r.Reporter.Errorf("Service %q has no parameters to edit", args.ID)
 		os.Exit(1)
 	}
 
@@ -112,11 +96,12 @@ func run(cmd *cobra.Command, argv []string) {
 		}
 	}
 
-	reporter.Debugf("Updating parameters for service %q", args.ID)
-	err = ocmClient.UpdateManagedService(args)
+	r.Reporter.Debugf("Updating parameters for service %q", args.ID)
+	err = r.OCMClient.UpdateManagedService(args)
 	if err != nil {
-		reporter.Errorf("Failed to update service %q: %v", args.ID, err)
+		r.Reporter.Errorf("Failed to update service %q: %v", args.ID, err)
 		os.Exit(1)
 	}
-	reporter.Infof("Service %q is now updating. To check the status run 'rosa describe service --id %s'", args.ID, args.ID)
+	r.Reporter.Infof("Service %q is now updating. To check the status run 'rosa describe service --id %s'",
+		args.ID, args.ID)
 }
