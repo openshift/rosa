aa0471b63c19f02b22dc3fe388f8ab0d582b6b1f
Guilherme Branco <gbranco@redhat.com>
2022-10-19 11:18:50 -0300

    feat: check if rosa cli is up to date

--- cmd/version/cmd.go
+++ cmd/version/cmd.go
@@ -17,10 +17,15 @@ limitations under the License.
 package version
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/zgalor/weberr"
 
 	"github.com/openshift/rosa/pkg/info"
 )
@@ -32,6 +37,37 @@ var Cmd = &cobra.Command{
 	Run:   run,
 }
 
+const (
+	releaseURL = "https://api.github.com/repos/openshift/rosa/releases/latest"
+)
+
 func run(cmd *cobra.Command, argv []string) {
 	fmt.Fprintf(os.Stdout, "%s\n", info.Version)
+	req, err := http.NewRequest("GET", releaseURL, nil)
+	if err != nil {
+		weberr.Errorf("Error setting up request for latest released rosa cli: %v", err)
+		os.Exit(1)
+	}
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		weberr.Errorf("Error while requesting latest released rosa cli: %v", err)
+		os.Exit(1)
+	}
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		weberr.Errorf("Error while requesting latest released rosa cli: %d %s", resp.StatusCode, resp.Status)
+		os.Exit(1)
+	}
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		weberr.Errorf("Error reading response body: %v", err)
+	}
+	result := make(map[string]interface{})
+	json.Unmarshal(respBody, &result)
+
+	if strings.Contains(result["tag_name"].(string), info.Version) {
+		fmt.Fprintf(os.Stdout, "Your ROSA CLI is up to date.\n")
+	} else {
+		fmt.Fprintf(os.Stdout, "There is a newer release version, please consider updating: %s\n", result["html_url"].(string))
+	}
 }
