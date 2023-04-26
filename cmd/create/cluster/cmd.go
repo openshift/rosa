869c4c47694202ad63b1bd560684ac04e34c8652
Will Kutler <wkutler@redhat.com>
2023-04-26 11:41:29 -0400

    SDA-8716 | Fix: provide warning message when no OIDC config is found
    Signed-off-by: Will Kutler <wkutler@redhat.com>

--- cmd/create/cluster/cmd.go
+++ cmd/create/cluster/cmd.go
@@ -2369,6 +2369,7 @@ func handleOidcConfigOptions(r *rosa.Runtime, cmd *cobra.Command, isSTS bool, is
 	}
 	if oidcConfigId == "" {
 		if !isHostedCP {
+			r.Reporter.Warnf("No OIDC Configuration found; will continue with the classic flow.")
 			return nil
 		}
 		if args.classicOidcConfig {
