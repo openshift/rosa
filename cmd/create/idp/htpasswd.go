dcd7dd76cc9902d58f9bd5ff6cc5235260811532
Will Kutler <wkutler@redhat.com>
2023-05-02 12:33:01 -0400

    SDA-8053 | Fix: make IDP password validation errors more readable
    ref: https://issues.redhat.com/browse/SDA-8053?filter=-1
    
    Signed-off-by: Will Kutler <wkutler@redhat.com>

--- cmd/create/idp/htpasswd.go
+++ cmd/create/idp/htpasswd.go
@@ -187,19 +187,35 @@ func usernameValidator(val interface{}) error {
 
 func passwordValidator(val interface{}) error {
 	if password, ok := val.(string); ok {
-		notAsciiOnly, _ := regexp.MatchString(`[^\x20-\x7E]`, password)
+		re := regexp.MustCompile(`[^\x20-\x7E]`)
+		invalidChars := re.FindAllString(password, -1)
+		notAsciiOnly := len(invalidChars) > 0
 		containsSpace := strings.Contains(password, " ")
 		tooShort := len(password) < 14
+		pwdErrors := []string{}
+		if notAsciiOnly {
+			pwdErrors = append(pwdErrors, fmt.Sprintf("must not contain special characters [%s]",
+				strings.Join(invalidChars, ", ")))
+		}
+		if containsSpace {
+			pwdErrors = append(pwdErrors, "must not contain whitespace")
+		}
+		if tooShort {
+			pwdErrors = append(pwdErrors, fmt.Sprintf("must be at least 14 characters (got %d)", len(password)))
+		}
 		if notAsciiOnly || containsSpace || tooShort {
-			return fmt.Errorf(
-				"password must be at least 14 characters (ASCII-standard) without whitespaces")
+			if len(pwdErrors) > 1 {
+				pwdErrors[len(pwdErrors)-1] = "and " + pwdErrors[len(pwdErrors)-1]
+			}
+
+			return fmt.Errorf("Password " + strings.Join(pwdErrors, ", "))
 		}
 		hasUppercase, _ := regexp.MatchString(`[A-Z]`, password)
 		hasLowercase, _ := regexp.MatchString(`[a-z]`, password)
 		hasNumberOrSymbol, _ := regexp.MatchString(`[^a-zA-Z]`, password)
 		if !hasUppercase || !hasLowercase || !hasNumberOrSymbol {
 			return fmt.Errorf(
-				"password must include uppercase letters, lowercase letters, and numbers " +
+				"Password must include uppercase letters, lowercase letters, and numbers " +
 					"or symbols (ASCII-standard characters only)")
 		}
 		return nil
