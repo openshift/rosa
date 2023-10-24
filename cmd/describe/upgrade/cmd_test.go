f4d355885eb1fd8d3a1516b851046224bd3e53c5
Maggie Chen <magchen@redhat.com>
2023-10-24 16:07:39 -0400

    OCM-3492 | fix: Classic upgrade should align left
    Signed-off-by: Maggie Chen <magchen@redhat.com>
    (cherry picked from commit 0ac36da1a0f36ac4067717e1cb396930fcb1c092)

--- cmd/describe/upgrade/cmd_test.go
+++ cmd/describe/upgrade/cmd_test.go
@@ -25,13 +25,13 @@ var _ = Describe("Describe upgrade", func() {
 			Expect(result).To(Equal(
 				fmt.Sprintf(
 					`
-		ID:                                id1
-		Cluster ID:                        
-		Schedule Type:                     
-		Next Run:                          %s
-		Upgrade State:                     scheduled
+ID:                                id1
+Cluster ID:                        
+Schedule Type:                     
+Next Run:                          %s
+Upgrade State:                     scheduled
 
-		Version:                           4.12.19
+Version:                           4.12.19
 `, nowUTC.Format("2006-01-02 15:04 MST"))))
 		})
 		It("Node pool upgrade is scheduled with a date", func() {
@@ -46,20 +46,69 @@ var _ = Describe("Describe upgrade", func() {
 			Expect(result).To(Equal(
 				fmt.Sprintf(
 					`
-		ID:                                id1
-		Cluster ID:                        
-		Schedule Type:                     
-		Next Run:                          %s
-		Upgrade State:                     scheduled
+ID:                                id1
+Cluster ID:                        
+Schedule Type:                     
+Next Run:                          %s
+Upgrade State:                     scheduled
 
-		Schedule At:                       %s
+Schedule At:                       %s
 
-		Enable minor version upgrades:     true
+Enable minor version upgrades:     true
 
-		Version:                           4.12.19
+Version:                           4.12.19
 `, nowUTC.Format("2006-01-02 15:04 MST"), nowUTC.Format("2006-01-02 15:04 MST"))))
 		})
 	})
+	Context("Describe Classic Upgrades", func() {
+		var testRuntime test.TestingRuntime
+		var clusterID = "cluster1"
+		var nodePoolID = "nodepool85"
+
+		version41224 := cmv1.NewVersion().ID("openshift-v4.12.24").RawID("4.12.24").ReleaseImage("1").
+			HREF("/api/clusters_mgmt/v1/versions/openshift-v4.12.24").Enabled(true).ChannelGroup("stable").
+			ROSAEnabled(true).HostedControlPlaneEnabled(true).AvailableUpgrades("4.12.25", "4.12.26")
+		nodePool, err := cmv1.NewNodePool().ID("workers").Replicas(2).AutoRepair(true).Version(version41224).Build()
+		Expect(err).To(BeNil())
+
+		upgradePolicies := make([]*cmv1.NodePoolUpgradePolicy, 0)
+		upgradePolicies = append(upgradePolicies, buildNodePoolUpgradePolicy())
+
+		BeforeEach(func() {
+			testRuntime.InitRuntime()
+		})
+		It("Upgrade policy found, no error", func() {
+			args.nodePool = nodePoolID
+			testRuntime.ApiServer.AppendHandlers(RespondWithJSON(http.StatusOK, test.FormatResource(nodePool)))
+			testRuntime.ApiServer.AppendHandlers(RespondWithJSON(http.StatusOK,
+				test.FormatNodePoolUpgradePolicyList(upgradePolicies)))
+			err := describeClassicUpgrades(testRuntime.RosaRuntime, clusterID)
+			Expect(err).To(BeNil())
+		})
+		It("Node pool upgrade is scheduled with a date", func() {
+			format.TruncatedDiff = false
+			nowUTC := time.Now().UTC()
+			upgradeState, err := cmv1.NewUpgradePolicyState().Value("scheduled").Build()
+			Expect(err).To(BeNil())
+			npUpgradePolicy, err := cmv1.NewUpgradePolicy().ID("id1").ClusterID("id1").
+				NextRun(nowUTC).Schedule(nowUTC.Format("2006-01-02 15:04 MST")).Version("4.12.25").Build()
+			Expect(err).To(BeNil())
+			result := formatClassicUpgrade(npUpgradePolicy, upgradeState)
+			Expect(result).To(Equal(
+				fmt.Sprintf(
+					`
+ID:                                id1
+Cluster ID:                        id1
+Next Run:                          %s
+Upgrade State:                     scheduled
+
+Schedule At:                       %s
+
+Version:                           4.12.25
+`, nowUTC.Format("2006-01-02 15:04 MST"), nowUTC.Format("2006-01-02 15:04 MST"))))
+		})
+	})
+
 	Context("Describe Hypershift upgrade", func() {
 		var testRuntime test.TestingRuntime
 		var clusterID = "cluster1"
