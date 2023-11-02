5a522b6c4c0a0bddcb43c1ff86d429071ef354f5
oriAdler <ooriAdler@gmail.com>
2023-11-02 10:52:01 +0200

    OCM-4644 | fix: Create cluster - filter classic ROSA account roles
    When the user creates a cluster in interactive mode and selecting
    classic ROSA topology, the CLI should display only classic account-roles.
    HCP account roles are not compatible with classic ROSA clusters.

---
+++ pkg/aws/policies_test.go
@@ -0,0 +1,61 @@
+package aws
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/iam"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Is Account Role Version Compatible", func() {
+	When("Role isn't an account role", func() {
+		It("Should return not compatible", func() {
+			isCompatible, err := isAccountRoleVersionCompatible([]*iam.Tag{}, InstallerAccountRole, "4.14")
+			Expect(err).To(BeNil())
+			Expect(isCompatible).To(Equal(false))
+		})
+	})
+	When("Role OCP version isn't compatible", func() {
+		It("Should return not compatible", func() {
+			tagsList := []*iam.Tag{
+				{
+					Key:   aws.String("rosa_openshift_version"),
+					Value: aws.String("4.13"),
+				},
+			}
+			isCompatible, err := isAccountRoleVersionCompatible(tagsList, InstallerAccountRole, "4.14")
+			Expect(err).To(BeNil())
+			Expect(isCompatible).To(Equal(false))
+		})
+	})
+	When("Role version is compatible", func() {
+		It("Should return compatible", func() {
+			tagsList := []*iam.Tag{
+				{
+					Key:   aws.String("rosa_openshift_version"),
+					Value: aws.String("4.14"),
+				},
+			}
+			isCompatible, err := isAccountRoleVersionCompatible(tagsList, InstallerAccountRole, "4.14")
+			Expect(err).To(BeNil())
+			Expect(isCompatible).To(Equal(true))
+		})
+	})
+	When("Role has managed policies, ignores openshift version", func() {
+		It("Should return compatible", func() {
+			tagsList := []*iam.Tag{
+				{
+					Key:   aws.String("rosa_openshift_version"),
+					Value: aws.String("4.12"),
+				},
+				{
+					Key:   aws.String("rosa_managed_policies"),
+					Value: aws.String("true"),
+				},
+			}
+			isCompatible, err := isAccountRoleVersionCompatible(tagsList, InstallerAccountRole, "4.14")
+			Expect(err).To(BeNil())
+			Expect(isCompatible).To(Equal(true))
+		})
+	})
+})
