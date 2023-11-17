c50f22280b63ddf2ffcb8563306609ed9338fb84
Guilherme Branco <gbranco@redhat.com>
2023-11-17 12:48:58 -0300

    OCM-4890 | fix: helper message infra/control sgs

--- cmd/create/cluster/cmd.go
+++ cmd/create/cluster/cmd.go
@@ -768,7 +768,7 @@ func init() {
 		&args.additionalInfraSecurityGroupIds,
 		securitygroups.InfraSecurityGroupFlag,
 		nil,
-		"The additional Security Group IDs to be added to the default infra machine pool. "+
+		"The additional Security Group IDs to be added to the infra worker nodes. "+
 			listInputMessage,
 	)
 
@@ -776,7 +776,7 @@ func init() {
 		&args.additionalControlPlaneSecurityGroupIds,
 		securitygroups.ControlPlaneSecurityGroupFlag,
 		nil,
-		"The additional Security Group IDs to be added to the default control plane machine pool. "+
+		"The additional Security Group IDs to be added to the control plane nodes. "+
 			listInputMessage,
 	)
 
