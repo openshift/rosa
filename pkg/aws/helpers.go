69c84c607ef20abb435e941cd4c2f728861ce1b5
Guilherme Branco <gbranco@redhat.com>
2024-01-08 10:49:36 -0300

    OCM-5397 | chore: bump ocm-commons

--- pkg/aws/helpers.go
+++ pkg/aws/helpers.go
@@ -412,9 +412,9 @@ func GetTagValues(tagsValue []iamtypes.Tag) (roleType string, version string) {
 	for _, tag := range tagsValue {
 		switch aws.ToString(tag.Key) {
 		case tags.RoleType:
-			roleType = aws.StringValue(tag.Value)
+			roleType = aws.ToString(tag.Value)
 		case awsCommonValidations.OpenShiftVersion:
-			version = aws.StringValue(tag.Value)
+			version = aws.ToString(tag.Value)
 		}
 	}
 	return
