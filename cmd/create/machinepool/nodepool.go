b10d91576f3c968642cecdde4135db9bd70ed40b
Tom McKay <thomasmckay@redhat.com>
2024-01-18 07:21:59 -0500

    OCM-5483 | fix: add cluster arg to help message

--- cmd/create/machinepool/nodepool.go
+++ cmd/create/machinepool/nodepool.go
@@ -394,8 +394,9 @@ func addNodePool(cmd *cobra.Command, clusterKey string, cluster *cmv1.Cluster, r
 		}
 	} else {
 		r.Reporter.Infof("Machine pool '%s' created successfully on hosted cluster '%s'", createdNodePool.ID(), clusterKey)
-		r.Reporter.Infof("To view the machine pool details, run 'rosa describe machinepool --machinepool %s'", name)
-		r.Reporter.Infof("To view all machine pools, run 'rosa list machinepools -c %s'", clusterKey)
+		r.Reporter.Infof("To view the machine pool details, run 'rosa describe machinepool --cluster %s --machinepool %s'",
+			clusterKey, name)
+		r.Reporter.Infof("To view all machine pools, run 'rosa list machinepools --cluster %s'", clusterKey)
 	}
 }
 
