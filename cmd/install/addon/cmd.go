7a9f9532866950bb17bb5eaf58c1e73839ad6b5b
tithakka <tithakka@redhat.com>
2024-01-22 09:26:58 -0800

    OCM-4963 | feat: Reverted golang version to 1.20

--- cmd/install/addon/cmd.go
+++ cmd/install/addon/cmd.go
@@ -18,6 +18,7 @@ package addon
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"os"
 	"regexp"
 	"strings"
@@ -28,7 +29,6 @@ import (
 	"github.com/spf13/cobra"
 	errors "github.com/zgalor/weberr"
 
-	awserr "github.com/openshift-online/ocm-common/pkg/aws/errors"
 	"github.com/openshift/rosa/pkg/arguments"
 	"github.com/openshift/rosa/pkg/aws"
 	"github.com/openshift/rosa/pkg/aws/tags"
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/openshift/rosa
 
-go 1.21
+go 1.20
 
 toolchain go1.21.3
 
