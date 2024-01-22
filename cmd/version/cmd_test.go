8939e616355d3a3b727d7f580867804f25416397
rblake <rblake@redhat.com>
2024-01-22 14:40:44 +0000

    OCM-5782 | feat: Print Git SHA as build information in rosa version

---
+++ cmd/version/cmd_test.go
@@ -0,0 +1,54 @@
+package version
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/openshift/rosa/pkg/info"
+	"github.com/spf13/cobra"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestVersionCommand(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "rosa version command")
+}
+
+var _ = Describe("Run Command", func() {
+
+	var delegateInvokeCount int
+	var buf *bytes.Buffer
+
+	BeforeEach(func() {
+		buf = new(bytes.Buffer)
+		writer = buf
+		delegateInvokeCount = 0
+		delegateCommand = func(cmd *cobra.Command, args []string) {
+			delegateInvokeCount++
+		}
+	})
+
+	When("Run in client-only mode", func() {
+		It("It only prints version and build information", func() {
+			args.clientOnly = true
+
+			Cmd.Execute()
+
+			Expect(buf.String()).To(Equal(fmt.Sprintf("%s (Build: %s)\n", info.Version, info.Build)))
+			Expect(delegateInvokeCount).To(Equal(0))
+		})
+	})
+
+	When("Run without client-only", func() {
+
+		It("Prints version information and invokes delegate command", func() {
+			args.clientOnly = false
+			Cmd.Execute()
+
+			Expect(buf.String()).To(Equal(fmt.Sprintf("%s (Build: %s)\n", info.Version, info.Build)))
+			Expect(delegateInvokeCount).To(Equal(1))
+		})
+	})
+})
