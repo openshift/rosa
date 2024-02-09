9911f0705b24744065f6cf7de0f0c1c2045893ec
David Lee <dle@redhat.com>
2024-02-09 18:50:56 -0500

    OCM-5972 | fix: triggering retries associated with 'InvalidClientTokenId'

--- pkg/aws/client.go
+++ pkg/aws/client.go
@@ -707,10 +707,13 @@ func (c *awsClient) ValidateCredentials() (bool, error) {
 	_, err := c.stsClient.GetCallerIdentity(context.Background(), &sts.GetCallerIdentityInput{})
 	if err != nil {
 		if strings.Contains(fmt.Sprintf("%s", err), "InvalidClientTokenId") {
-			return false, fmt.Errorf(
-				"Invalid AWS Credentials. For help configuring your credentials, see " +
-					"https://docs.openshift.com/rosa/rosa_install_access_delete_clusters/rosa_getting_started_iam/" +
-					"rosa-config-aws-account.html#rosa-configuring-aws-account_rosa-config-aws-account")
+			awsErr := awserr.New("InvalidClientTokenId",
+				"Invalid AWS Credentials. For help configuring your credentials, see "+
+					"https://docs.openshift.com/rosa/rosa_install_access_delete_clusters/rosa_getting_started_iam/"+
+					"rosa-config-aws-account.html#rosa-configuring-aws-account_rosa-config-aws-account",
+				err)
+			return false, awsErr
+
 		}
 		return false, err
 	}
