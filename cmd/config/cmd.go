8043960bb1398e70731b1f6f78bf4b3a28bb0b26
tithakka <tithakka@redhat.com>
2024-02-27 12:29:50 -0800

    OCM-5804 | feat: Add token and config commands

---
+++ cmd/config/cmd.go
@@ -0,0 +1,76 @@
+/*
+Copyright (c) 2024 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/openshift/rosa/cmd/config/get"
+	"github.com/openshift/rosa/cmd/config/set"
+	"github.com/openshift/rosa/pkg/config"
+)
+
+func configVarDocs() (ret string) {
+	configType := reflect.ValueOf(config.Config{}).Type()
+	fieldHelps := make([]string, configType.NumField())
+	for i := 0; i < len(fieldHelps); i++ {
+		tag := configType.Field(i).Tag
+		name := strings.Split(tag.Get("json"), ",")[0]
+		doc := tag.Get("doc")
+		fieldHelps[i] = fmt.Sprintf("\t%-15s%s", name, doc)
+	}
+	ret = strings.Join(fieldHelps, "\n")
+	return
+}
+
+func longHelp() (ret string) {
+	loc, err := config.Location()
+	if err != nil {
+		// I think this only happens if homedir.Dir() fails, which is unlikely.
+		loc = fmt.Sprintf("UNKNOWN (%s)", err)
+	}
+	ret = fmt.Sprintf(`Get or set variables from a configuration file.
+
+The location of the configuration file is gleaned from the 'OCM_CONFIG' environment variable,
+or ~/.ocm.json if that variable is unset. Currently using: %s
+
+The following variables are supported:
+
+%s
+
+Note that "rosa config get access_token" gives whatever the file contains - may be missing or expired;
+you probably want "rosa token" command instead which will obtain a fresh token if needed.
+`, loc, configVarDocs())
+	return
+}
+
+func NewConfigCommand() *cobra.Command {
+	Cmd := &cobra.Command{
+		Use:   "config COMMAND VARIABLE",
+		Short: "get or set configuration variables",
+		Long:  longHelp(),
+	}
+	Cmd.AddCommand(get.Cmd)
+	Cmd.AddCommand(set.Cmd)
+	return Cmd
+}
+
+var Cmd = NewConfigCommand()
---
+++ cmd/config/cmd_test.go
@@ -0,0 +1,154 @@
+package config
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/openshift/rosa/cmd/config/get"
+	"github.com/openshift/rosa/cmd/config/set"
+	"github.com/openshift/rosa/pkg/config"
+	"github.com/openshift/rosa/pkg/test"
+)
+
+func TestConfigCommand(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "rosa token command")
+}
+
+var _ = Describe("Run Command", Ordered, func() {
+	var testRuntime test.TestingRuntime
+	var buf *bytes.Buffer
+	var tmpdir string
+	var err error
+
+	BeforeAll(func() {
+		tmpdir, err = os.MkdirTemp("/tmp", ".ocm-config-*")
+		os.Setenv("OCM_CONFIG", tmpdir+"/ocm_config.json")
+	})
+
+	AfterAll(func() {
+		os.Setenv("OCM_CONFIG", "")
+	})
+
+	BeforeEach(func() {
+		buf = new(bytes.Buffer)
+		get.Writer = buf
+		testRuntime.InitRuntime()
+	})
+
+	It("Saves config", func() {
+		accessToken := "MyTestToken"
+		err = set.SaveConfig("access_token", accessToken)
+		Expect(err).To(BeNil())
+		currentConfig, err := config.Load()
+		Expect(err).To(BeNil())
+		Expect(currentConfig.AccessToken).To(Equal(accessToken))
+
+		clientId := "MyClientId"
+		err = set.SaveConfig("client_id", clientId)
+		Expect(err).To(BeNil())
+		currentConfig, err = config.Load()
+		Expect(err).To(BeNil())
+		Expect(currentConfig.ClientID).To(Equal(clientId))
+
+		insecure := "true"
+		err = set.SaveConfig("insecure", insecure)
+		Expect(err).To(BeNil())
+		currentConfig, err = config.Load()
+		Expect(err).To(BeNil())
+		Expect(strconv.FormatBool(currentConfig.Insecure)).To(Equal(insecure))
+
+		refreshToken := "MyRefreshToken"
+		err = set.SaveConfig("refresh_token", refreshToken)
+		Expect(err).To(BeNil())
+		currentConfig, err = config.Load()
+		Expect(err).To(BeNil())
+		Expect(currentConfig.RefreshToken).To(Equal(refreshToken))
+
+		scopes := "MyScopes"
+		err = set.SaveConfig("scopes", scopes)
+		Expect(err).NotTo(BeNil())
+		Expect(err.Error()).To(Equal("Setting scopes is unsupported"))
+
+		tokenUrl := "MyTokenURL"
+		err = set.SaveConfig("token_url", tokenUrl)
+		Expect(err).To(BeNil())
+		currentConfig, err = config.Load()
+		Expect(err).To(BeNil())
+		Expect(currentConfig.TokenURL).To(Equal(tokenUrl))
+
+		url := "MyURL"
+		err = set.SaveConfig("url", url)
+		Expect(err).To(BeNil())
+		currentConfig, err = config.Load()
+		Expect(err).To(BeNil())
+		Expect(currentConfig.URL).To(Equal(url))
+
+		fedramp := "true"
+		err = set.SaveConfig("fedramp", fedramp)
+		Expect(err).To(BeNil())
+		currentConfig, err = config.Load()
+		Expect(err).To(BeNil())
+		Expect(strconv.FormatBool(currentConfig.FedRAMP)).To(Equal(fedramp))
+
+		insecure = "Incorrect"
+		err = set.SaveConfig("insecure", insecure)
+		Expect(err).NotTo(BeNil())
+		Expect(err.Error()).To(ContainSubstring("Failed to set insecure"))
+
+		randomField := "random value"
+		err = set.SaveConfig("random_field", randomField)
+		Expect(err).NotTo(BeNil())
+		Expect(err.Error()).To(Equal("Unknown setting"))
+	})
+
+	It("Prints config", func() {
+		currentConfig, err := config.Load()
+		Expect(err).To(BeNil())
+		err = get.PrintConfig("access_token")
+		Expect(err).To(BeNil())
+		Expect(buf.String()).To(ContainSubstring(currentConfig.AccessToken))
+
+		err = get.PrintConfig("client_id")
+		Expect(err).To(BeNil())
+		Expect(buf.String()).To(ContainSubstring(currentConfig.ClientID))
+
+		err = get.PrintConfig("client_secret")
+		Expect(err).To(BeNil())
+		Expect(buf.String()).To(ContainSubstring(currentConfig.ClientSecret))
+
+		err = get.PrintConfig("insecure")
+		Expect(err).To(BeNil())
+		Expect(buf.String()).To(ContainSubstring(strconv.FormatBool(currentConfig.Insecure)))
+
+		err = get.PrintConfig("refresh_token")
+		Expect(err).To(BeNil())
+		Expect(buf.String()).To(ContainSubstring(currentConfig.RefreshToken))
+
+		err = get.PrintConfig("scopes")
+		Expect(err).To(BeNil())
+		Expect(buf.String()).To(ContainSubstring(fmt.Sprint(currentConfig.Scopes)))
+
+		err = get.PrintConfig("token_url")
+		Expect(err).To(BeNil())
+		Expect(buf.String()).To(ContainSubstring(currentConfig.TokenURL))
+
+		err = get.PrintConfig("url")
+		Expect(err).To(BeNil())
+		Expect(buf.String()).To(ContainSubstring(currentConfig.URL))
+
+		err = get.PrintConfig("fedramp")
+		Expect(err).To(BeNil())
+		Expect(buf.String()).To(ContainSubstring(strconv.FormatBool(currentConfig.FedRAMP)))
+
+		err = get.PrintConfig("test")
+		Expect(err).NotTo(BeNil())
+		Expect(err.Error()).To(Equal("Unknown setting"))
+	})
+})
