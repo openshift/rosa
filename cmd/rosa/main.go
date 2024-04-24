a0b5f77d48da4d94458c2a2082ca790c86ecec76
croche <croche@redhat.com>
2024-04-24 11:00:30 +0100

    OCM-7573 | fix: ensure version option args are initialised

--- cmd/rosa/main.go
+++ cmd/rosa/main.go
@@ -127,21 +127,21 @@ func versionCheck(cmd *cobra.Command, _ []string) {
 	rprtr := reporter.CreateReporter()
 	rosaVersion, err := versionUtils.NewRosaVersion()
 	if err != nil {
-		rprtr.Warnf("Could not verify the current version of ROSA.")
-		rprtr.Warnf("You might be running on an outdated version. Make sure you are using the current version of ROSA.")
+		rprtr.Debugf("Could not verify the current version of ROSA: %v", err)
+		rprtr.Debugf("You might be running on an outdated version. Make sure you are using the current version of ROSA.")
 		return
 	}
 	latestVersionFromMirror, isLatest, err := rosaVersion.IsLatest(info.Version)
 	if err != nil {
-		rprtr.Warnf("There was a problem retrieving the latest version of ROSA.")
-		rprtr.Warnf("You might be running on an outdated version. Make sure you are using the current version of ROSA.")
+		rprtr.Debugf("There was a problem retrieving the latest version of ROSA: %v", err)
+		rprtr.Debugf("You might be running on an outdated version. Make sure you are using the current version of ROSA.")
+		return
 	}
 	if !isLatest {
 		rprtr.Warnf("The current version (%s) is not up to date with latest released version (%s).",
 			info.Version,
 			latestVersionFromMirror.Original(),
 		)
-
 		rprtr.Warnf("It is recommended that you update to the latest version.")
 	}
 }
