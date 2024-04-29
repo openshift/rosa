aae814bd9f3e182008ba1ab72a0d2497a79f907c
Maggie Chen <magchen@redhat.com>
2024-04-29 14:40:38 -0400

    OCM-7535 | fix: change not found to info
    Signed-off-by: Maggie Chen <magchen@redhat.com>
    
    rephrase
    
    Signed-off-by: Maggie Chen <magchen@redhat.com>
    
    fmt
    
    Signed-off-by: Maggie Chen <magchen@redhat.com>
    
    change text
    
    Signed-off-by: Maggie Chen <magchen@redhat.com>
    
    fix text
    
    Signed-off-by: Maggie Chen <magchen@redhat.com>

---
+++ cmd/list/breakglasscredential/breakglasscredential_suite_test.go
@@ -0,0 +1,13 @@
+package breakglasscredential_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestBreakGlassCredential(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Breakglasscredential Suite")
+}
