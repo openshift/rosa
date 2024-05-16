fa2b0afdbd5c6f9e8a88f950d9c562215d3f7204
xueli181114 <xueli@redhat.com>
2024-05-16 10:38:49 +0800

    OCM-8045 | test: Automated cases id:73753,73754 for kubeletconfig porting on HCP and update id:68828,68835,68836,64767

--- tests/utils/exec/rosacli/kubelet_config_service.go
+++ tests/utils/exec/rosacli/kubelet_config_service.go
@@ -5,14 +5,16 @@ import (
 
 	"gopkg.in/yaml.v3"
 
-	. "github.com/openshift/rosa/tests/utils/log"
+	"github.com/openshift/rosa/tests/utils/log"
 )
 
 type KubeletConfigService interface {
 	ResourcesCleaner
-
-	DescribeKubeletConfig(clusterID string) (bytes.Buffer, error)
-	ReflectKubeletConfigDescription(result bytes.Buffer) *KubeletConfigDescription
+	ListKubeletConfigs(clusterID string, flags ...string) (bytes.Buffer, error)
+	ListKubeletConfigsAndReflect(clusterID string, flags ...string) (kubes *KubeletConfigList, err error)
+	ReflectKubeletConfigs(output bytes.Buffer) (kubes *KubeletConfigList, err error)
+	DescribeKubeletConfig(clusterID string, flags ...string) (bytes.Buffer, error)
+	ReflectKubeletConfig(result bytes.Buffer) *KubeletConfig
 	EditKubeletConfig(clusterID string, flags ...string) (bytes.Buffer, error)
 	DeleteKubeletConfig(clusterID string, flags ...string) (bytes.Buffer, error)
 	CreateKubeletConfig(clusterID string, flags ...string) (bytes.Buffer, error)
@@ -33,23 +35,81 @@ func NewKubeletConfigService(client *Client) KubeletConfigService {
 	}
 }
 
-// Struct for the 'rosa describe kubeletconfig' output
-type KubeletConfigDescription struct {
-	PodPidsLimit int `yaml:"Pod Pids Limit,omitempty"`
+// Struct for the 'rosa describe/list kubeletconfig(s)' output
+type KubeletConfig struct {
+	ID           string `yaml:"ID,omitempty" json:"ID,omitempty"`
+	Name         string `yaml:"Name,omitempty" json:"NAME,omitempty"`
+	PodPidsLimit string `yaml:"Pod Pids Limit,omitempty" json:"POD PIDS LIMIT,omitempty"`
+}
+
+// Struct for the 'rosa list kubeletconfigs'
+type KubeletConfigList struct {
+	KubeletConfigs []*KubeletConfig
+}
+
+func (kl *KubeletConfigList) KubeletConfig(kubeName string) *KubeletConfig {
+	for _, kubeletconfig := range kl.KubeletConfigs {
+		if kubeletconfig.Name == kubeName {
+			return kubeletconfig
+		}
+	}
+	return nil
+}
+
+// List kubeletconfigs
+func (k *kubeletConfigService) ListKubeletConfigs(clusterID string, flags ...string) (bytes.Buffer, error) {
+	combflags := append([]string{"-c", clusterID}, flags...)
+	list := k.client.Runner.
+		Cmd("list", "kubeletconfigs").
+		CmdFlags(combflags...)
+
+	return list.Run()
+}
+
+// Reflect kubeletconfigs
+func (k *kubeletConfigService) ReflectKubeletConfigs(output bytes.Buffer) (kubes *KubeletConfigList, err error) {
+	kubes = &KubeletConfigList{}
+	theMap := k.client.Parser.TableData.Input(output).Parse().Output()
+	for _, kubeletConfigItem := range theMap {
+		kube := &KubeletConfig{}
+		err = MapStructure(kubeletConfigItem, kube)
+		if err != nil {
+			return
+		}
+		kubes.KubeletConfigs = append(kubes.KubeletConfigs, kube)
+	}
+	return kubes, err
+}
+
+// ListKubeletConfigsAndReflect will list the kubeletconfigs
+func (k *kubeletConfigService) ListKubeletConfigsAndReflect(clusterID string, flags ...string) (kubes *KubeletConfigList, err error) {
+	var output bytes.Buffer
+	output, err = k.ListKubeletConfigs(clusterID, flags...)
+	if err != nil {
+		return
+	}
+	kubes, err = k.ReflectKubeletConfigs(output)
+	return
+}
+
+// List and reflect kubeletconfigs
+func (k *kubeletConfigService) ListAndReflectKubeletConfigs(clusterID string, flags ...string) {
+
 }
 
 // Describe Kubeletconfig
-func (k *kubeletConfigService) DescribeKubeletConfig(clusterID string) (bytes.Buffer, error) {
+func (k *kubeletConfigService) DescribeKubeletConfig(clusterID string, flags ...string) (bytes.Buffer, error) {
+	cmdflags := append([]string{"-c", clusterID}, flags...)
 	describe := k.client.Runner.
 		Cmd("describe", "kubeletconfig").
-		CmdFlags("-c", clusterID)
+		CmdFlags(cmdflags...)
 
 	return describe.Run()
 }
 
-// Pasrse the result of 'rosa describe kubeletconfig' to the KubeletConfigDescription struct
-func (k *kubeletConfigService) ReflectKubeletConfigDescription(result bytes.Buffer) *KubeletConfigDescription {
-	res := new(KubeletConfigDescription)
+// Pasrse the result of 'rosa describe kubeletconfig' to the KubeletConfig struct
+func (k *kubeletConfigService) ReflectKubeletConfig(result bytes.Buffer) *KubeletConfig {
+	res := new(KubeletConfig)
 	theMap, _ := k.client.Parser.TextData.Input(result).Parse().YamlToMap()
 	data, _ := yaml.Marshal(&theMap)
 	yaml.Unmarshal(data, res)
@@ -93,11 +153,14 @@ func (k *kubeletConfigService) CreateKubeletConfig(clusterID string, flags ...st
 
 func (k *kubeletConfigService) CleanResources(clusterID string) (errors []error) {
 	if k.created[clusterID] {
-		Logger.Infof("Remove remaining kubelet config")
-		_, err := k.DeleteKubeletConfig(clusterID)
+		log.Logger.Infof("Remove remaining kubelet config")
+		_, err := k.DeleteKubeletConfig(clusterID,
+			"-y",
+		)
 		if err != nil {
 			errors = append(errors, err)
 		}
 	}
+
 	return
 }
