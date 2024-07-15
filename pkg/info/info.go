2fb09738894817301433914904d541431a4aa662
Marco <llan@redhat.com>
2024-07-15 08:24:45 -0700

    OCM-9494 | chore: Set release version to 1.2.42

--- pkg/info/info.go
+++ pkg/info/info.go
@@ -18,7 +18,7 @@ limitations under the License.
 
 package info
 
-const Version = "1.2.42-RC1"
+const Version = "1.2.42"
 
 // Build contains the short Git SHA of the CLI at the point it was build. Set via `-ldflags` at build time
 var Build = "local"
