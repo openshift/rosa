d806aa137874ffd26ad4398bcf763df154f6f82a
Jeff Frazier <jfrazier@redhat.com>
2024-08-08 10:48:49 -0400

    OCM-10248 | test: automated cases id:75052

--- tests/e2e/test_rosacli_ingress.go
+++ tests/e2e/test_rosacli_ingress.go
@@ -172,7 +172,7 @@ var _ = Describe("Edit default ingress",
 					"ERR: Cluster name, identifier or external identifier '' " +
 						"isn't valid: it must contain only letters, digits, dashes and underscore"))
 
-				By("Get default ingress id with no clusterID provided")
+				By("Get cluster ingress with invalid/non-existing cluster id")
 				out, err := rosaClient.Ingress.DescribeIngress(clusterID, "xxx")
 				Expect(err).To(HaveOccurred())
 				Expect(out.String()).Should(ContainSubstring("ERR: Failed to get ingress 'xxx' for cluster '%s'", clusterID))
