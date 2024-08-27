97919e68545d4e824e10cc100222b668e2b060a8
Ciaran Roche <croche@redhat.com>
2024-08-27 10:47:23 +0100

    OCM-9503 | chore: bump version for 1.2.44

--- pkg/info/info.go
+++ pkg/info/info.go
@@ -18,7 +18,7 @@ limitations under the License.
 
 package info
 
-const Version = "1.2.44-RC1"
+const Version = "1.2.44"
 
 // Build contains the short Git SHA of the CLI at the point it was build. Set via `-ldflags` at build time
 var Build = "local"
