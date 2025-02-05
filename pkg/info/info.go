8edd131b650fbb28eefcb47cd10345cad455417a
hunterkepley <hkepley@redhat.com>
2025-02-05 13:53:00 -0500

    OCM-13520 | chore: Set release version to 1.2.50-rc3

--- pkg/info/info.go
+++ pkg/info/info.go
@@ -18,7 +18,7 @@ limitations under the License.
 
 package info
 
-const DefaultVersion = "1.2.50-RC2"
+const DefaultVersion = "1.2.50-RC3"
 
 // Build contains the short Git SHA of the CLI at the point it was build. Set via `-ldflags` at build time
 var Build = "local"
