54d6bdc0adbd4a5365df2fbe8aafb0ef9815eeb3
yuwang-RH <yuwan@redhat.com>
2025-06-04 19:38:29 +0800

    OCM-15880 | ci: Create new dockerfile for e2e testing component

--- hack/build_cli.sh
+++ hack/build_cli.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #This script invoked via a make target by the Dockerfile
 #which builds a cli wrapper container that contains all release images
-archs=(amd64)
+archs=(amd64 arm64)
 oses=(darwin linux windows)
 
 mkdir -p releases
@@ -14,10 +14,10 @@ do
     if [[ $os == "windows" ]]; then
         extension=".exe"
     fi
-    GOOS=${os} GOARCH=${arch} go build -o /tmp/ocm_${os}_${arch} ./cmd/ocm
-    mv /tmp/ocm_${os}_${arch} ocm${extension}
-    zip releases/ocm_${os}_${arch}.zip ocm${extension}
-    rm ocm${extension}
+    GOOS=${os} GOARCH=${arch} go build -o /tmp/rosa_${os}_${arch} ./cmd/rosa
+    mv /tmp/rosa_${os}_${arch} rosa${extension}
+    zip releases/rosa_${os}_${arch}.zip rosa${extension}
+    rm rosa${extension}
   done
 done
 cd releases
