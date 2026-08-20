82c7fc30e31e9645b05a179ec104d85a85f9a8aa
Lucas Freitas <lufreita@redhat.com>
2026-08-20 11:47:36 -0300

    ROSAENG-5657 | chore: add zip archives for Konflux GitHub releases (#3478)

--- hack/build_cli.sh
+++ hack/build_cli.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+
 #This script invoked via a make target by the Dockerfile
 #which builds a cli wrapper container that contains all release images
 archs=(amd64 arm64)
@@ -16,9 +18,15 @@ do
         extension=".exe"
     fi
     tmpdir=$(mktemp -d)
+    trap 'rm -rf "$tmpdir"' EXIT
     GOOS="${os}" GOARCH="${arch}" go build -ldflags="-X github.com/openshift/rosa/pkg/info.Build=$(git rev-parse --short HEAD)" -o "${tmpdir}/rosa${extension}" ./cmd/rosa
     tar -czf "releases/rosa_${os}_${arch}.tar.gz" -C "${tmpdir}" "rosa${extension}"
+    (
+      cd "${tmpdir}" && \
+      zip -r "${OLDPWD}/releases/rosa_${os}_${arch}.zip" "rosa${extension}"
+    )
     rm -rf "${tmpdir}"
+    trap - EXIT
   done
 done
 }
