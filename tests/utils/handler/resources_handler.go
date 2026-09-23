67d50758924060a36017bd329655759a675dd918
Mark Irish <mirish@redhat.com>
2026-09-23 11:49:42 -0500

    ROSAENG-64518 | fix: Fix logic for tearing down shared VPC resources to fix (#3575)
    Fixes that a security group in the shared VPC, owned by the primary/cluster account, can't be deleted by the shared/prefix account's credentials that DeleteVPCChain uses
    Fixes DeleteVpcEndpoints returns before AWS finishes detaching the ENIs, so the immediately-following SG/ENI/subnet/VPC deletes fail with
         DependencyViolation/InvalidParameterValue
    Add additional tests

--- tests/utils/handler/resources_handler.go
+++ tests/utils/handler/resources_handler.go
@@ -607,6 +607,9 @@ func (rh *resourcesHandler) GetAWSClient(useSharedAccount bool) (*aws_client.AWS
 		}
 		return aws_client.CreateAWSClient("", rh.resources.Region, rh.awsSharedAccountCredentialsFile)
 	}
+	if rh.awsCredentialsFile != "" {
+		return aws_client.CreateAWSClient("", rh.resources.Region, rh.awsCredentialsFile)
+	}
 	return aws_client.CreateAWSClient("", rh.resources.Region)
 }
 
--- tests/utils/handler/resources_handler_clean_test.go
+++ tests/utils/handler/resources_handler_clean_test.go
@@ -5,10 +5,16 @@ package handler
 
 import (
 	"fmt"
+	"time"
 
+	"go.uber.org/mock/gomock"
+
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/aws/smithy-go"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/openshift-online/ocm-common/pkg/aws/aws_client"
 )
 
 var _ = Describe("isAWSAuthorizationError", func() {
@@ -58,5 +64,157 @@ var _ = Describe("hostedZoneIsHCPInternal", func() {
 
 	It("returns false when hypershift.local appears mid-string", func() {
 		Expect(hostedZoneIsHCPInternal("hypershift.local.example.com")).To(BeFalse())
+	})
+})
+
+var _ = Describe("waitForVPCEndpointENIsClearedWithTimeout", func() {
+	var (
+		ctrl      *gomock.Controller
+		mockEc2   *aws_client.MockEC2ClientAPI
+		awsclient *aws_client.AWSClient
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		mockEc2 = aws_client.NewMockEC2ClientAPI(ctrl)
+		awsclient = &aws_client.AWSClient{Ec2Client: mockEc2}
+	})
+
+	AfterEach(func() {
+		ctrl.Finish()
+	})
+
+	It("returns immediately when there are no vpc endpoints or lingering ENIs", func() {
+		mockEc2.EXPECT().DescribeVpcEndpoints(gomock.Any(), gomock.Any()).
+			Return(&ec2.DescribeVpcEndpointsOutput{}, nil)
+		mockEc2.EXPECT().DescribeNetworkInterfaces(gomock.Any(), gomock.Any()).
+			Return(&ec2.DescribeNetworkInterfacesOutput{}, nil)
+
+		err := waitForVPCEndpointENIsClearedWithTimeout(awsclient, "vpc-123", time.Millisecond, 50*time.Millisecond)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("deletes vpc endpoints and waits for their ENIs to clear", func() {
+		endpointID := "vpce-123"
+		eniID := "eni-123"
+		mockEc2.EXPECT().DescribeVpcEndpoints(gomock.Any(), gomock.Any()).
+			Return(&ec2.DescribeVpcEndpointsOutput{
+				VpcEndpoints: []types.VpcEndpoint{{VpcEndpointId: &endpointID}},
+			}, nil)
+		mockEc2.EXPECT().DeleteVpcEndpoints(gomock.Any(), gomock.Any()).
+			Return(&ec2.DeleteVpcEndpointsOutput{}, nil)
+
+		gomock.InOrder(
+			mockEc2.EXPECT().DescribeNetworkInterfaces(gomock.Any(), gomock.Any()).
+				Return(&ec2.DescribeNetworkInterfacesOutput{
+					NetworkInterfaces: []types.NetworkInterface{
+						{NetworkInterfaceId: &eniID, InterfaceType: types.NetworkInterfaceTypeVpcEndpoint},
+					},
+				}, nil),
+			mockEc2.EXPECT().DescribeNetworkInterfaces(gomock.Any(), gomock.Any()).
+				Return(&ec2.DescribeNetworkInterfacesOutput{}, nil),
+		)
+
+		err := waitForVPCEndpointENIsClearedWithTimeout(awsclient, "vpc-123", 10*time.Millisecond, time.Second)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("times out if the endpoint ENI never clears", func() {
+		eniID := "eni-999"
+		mockEc2.EXPECT().DescribeVpcEndpoints(gomock.Any(), gomock.Any()).
+			Return(&ec2.DescribeVpcEndpointsOutput{}, nil)
+		mockEc2.EXPECT().DescribeNetworkInterfaces(gomock.Any(), gomock.Any()).
+			Return(&ec2.DescribeNetworkInterfacesOutput{
+				NetworkInterfaces: []types.NetworkInterface{
+					{NetworkInterfaceId: &eniID, InterfaceType: types.NetworkInterfaceTypeVpcEndpoint},
+				},
+			}, nil).AnyTimes()
+
+		err := waitForVPCEndpointENIsClearedWithTimeout(awsclient, "vpc-123", 5*time.Millisecond, 30*time.Millisecond)
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("ignores ENIs that are not vpc endpoint interfaces", func() {
+		eniID := "eni-normal"
+		mockEc2.EXPECT().DescribeVpcEndpoints(gomock.Any(), gomock.Any()).
+			Return(&ec2.DescribeVpcEndpointsOutput{}, nil)
+		mockEc2.EXPECT().DescribeNetworkInterfaces(gomock.Any(), gomock.Any()).
+			Return(&ec2.DescribeNetworkInterfacesOutput{
+				NetworkInterfaces: []types.NetworkInterface{
+					{NetworkInterfaceId: &eniID, InterfaceType: types.NetworkInterfaceTypeInterface},
+				},
+			}, nil)
+
+		err := waitForVPCEndpointENIsClearedWithTimeout(awsclient, "vpc-123", time.Millisecond, 50*time.Millisecond)
+		Expect(err).ToNot(HaveOccurred())
+	})
+})
+
+var _ = Describe("deleteForeignOwnedSecurityGroupsWithClient", func() {
+	var (
+		ctrl      *gomock.Controller
+		mockEc2   *aws_client.MockEC2ClientAPI
+		awsclient *aws_client.AWSClient
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		mockEc2 = aws_client.NewMockEC2ClientAPI(ctrl)
+		awsclient = &aws_client.AWSClient{Ec2Client: mockEc2}
+	})
+
+	AfterEach(func() {
+		ctrl.Finish()
+	})
+
+	It("deletes every security group it owns", func() {
+		sgID1, sgID2 := "sg-owned-1", "sg-owned-2"
+		mockEc2.EXPECT().DescribeSecurityGroups(gomock.Any(), gomock.Any()).
+			Return(&ec2.DescribeSecurityGroupsOutput{
+				SecurityGroups: []types.SecurityGroup{
+					{GroupId: &sgID1, GroupName: &sgID1, Description: &sgID1},
+					{GroupId: &sgID2, GroupName: &sgID2, Description: &sgID2},
+				},
+			}, nil)
+		mockEc2.EXPECT().DescribeSecurityGroupRules(gomock.Any(), gomock.Any()).
+			Return(&ec2.DescribeSecurityGroupRulesOutput{}, nil).Times(2)
+		mockEc2.EXPECT().DeleteSecurityGroup(gomock.Any(), gomock.Any()).
+			Return(&ec2.DeleteSecurityGroupOutput{}, nil)
+		mockEc2.EXPECT().DeleteSecurityGroup(gomock.Any(), gomock.Any()).
+			Return(&ec2.DeleteSecurityGroupOutput{}, nil)
+
+		err := deleteForeignOwnedSecurityGroupsWithClient(awsclient, "vpc-123")
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("tolerates a foreign-owned security group and keeps deleting the rest", func() {
+		ownedID, foreignID := "sg-owned", "sg-foreign"
+		mockEc2.EXPECT().DescribeSecurityGroups(gomock.Any(), gomock.Any()).
+			Return(&ec2.DescribeSecurityGroupsOutput{
+				SecurityGroups: []types.SecurityGroup{
+					{GroupId: &foreignID, GroupName: &foreignID, Description: &foreignID},
+					{GroupId: &ownedID, GroupName: &ownedID, Description: &ownedID},
+				},
+			}, nil)
+		mockEc2.EXPECT().DescribeSecurityGroupRules(gomock.Any(), gomock.Any()).
+			Return(&ec2.DescribeSecurityGroupRulesOutput{}, nil).Times(2)
+		mockEc2.EXPECT().DeleteSecurityGroup(gomock.Any(), gomock.Any()).
+			Return(nil, &smithy.GenericAPIError{
+				Code:    "UnauthorizedOperation",
+				Message: "A subnet in this vpc is shared but the provided object is not owned by you",
+			})
+		mockEc2.EXPECT().DeleteSecurityGroup(gomock.Any(), gomock.Any()).
+			Return(&ec2.DeleteSecurityGroupOutput{}, nil)
+
+		err := deleteForeignOwnedSecurityGroupsWithClient(awsclient, "vpc-123")
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("propagates a failure to list security groups", func() {
+		mockEc2.EXPECT().DescribeSecurityGroups(gomock.Any(), gomock.Any()).
+			Return(nil, fmt.Errorf("boom"))
+
+		err := deleteForeignOwnedSecurityGroupsWithClient(awsclient, "vpc-123")
+		Expect(err).To(HaveOccurred())
 	})
 })
